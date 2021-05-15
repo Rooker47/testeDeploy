167c8ef2ad9f18a5793c1f15d3550b273a175d85
Celso Silva <cjrti95@gmail.com>
2021-05-15 12:45:53 -0300

    Iniciando o upload

--- testeDeploy/settings.py
+++ testeDeploy/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'appOlaMundo.apps.AppolamundoConfig'
+    'appOlaMundo.apps.AppolamundoConfig',
 ]
 
 MIDDLEWARE = [
